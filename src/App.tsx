a5409c7f3e926230341354a08cbedb6daf3c7d32
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-11-09 14:26:18 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <p>pagina de teste</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate vitae illo provident minus beatae accusantium itaque voluptates at, est neque ullam ratione, tenetur culpa in facere corrupti, aspernatur earum ipsa?</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate vitae illo provident minus beatae accusantium itaque voluptates at, est neque ullam ratione, tenetur culpa in facere corrupti, aspernatur earum ipsa?</p>
-   
+   <p>GITOCA</p>
     </>
   )
 }
