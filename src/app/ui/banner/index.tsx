1d3d82bfb0e21f11e8044e2a0b0404c67e350383
ilgnerbecheleni <ilgner.gui@gmail.com>
2025-02-24 21:23:53 -0300

    img

--- src/app/ui/banner/index.tsx
+++ src/app/ui/banner/index.tsx
@@ -1,20 +1,25 @@
+import Image from "next/image";
+
 interface BannerProps {
-    imageUrl: string;
-    title: string;
-    subtitle: string;
-  }
-  
-  export default function Banner({ imageUrl, title, subtitle }: BannerProps) {
-    return (
-      <div 
-        className="bg-cover bg-center h-[500px] flex items-center justify-center"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      >
-        <div className="bg-black bg-opacity-50 p-4 rounded-md text-center">
-          <h1 className="text-white text-4xl font-bold mb-2">{title}</h1>
-          <p className="text-white text-lg">{subtitle}</p>
-        </div>
+  imageUrl: string;
+  title: string;
+  subtitle: string;
+}
+import image from '/public/mundo.jpg'
+export default function Banner({ imageUrl, title, subtitle }: BannerProps) {
+  return (
+    <div className="relative h-[500px] flex items-center justify-center">
+      <Image
+        src={image}
+        alt={title}
+        layout="fill"
+        objectFit="cover"
+        className="absolute z-0"
+      />
+      <div className="relative bg-black bg-opacity-50 p-4 rounded-md text-center z-10">
+        <h1 className="text-white text-4xl font-bold mb-2">{title}</h1>
+        <p className="text-white text-lg">{subtitle}</p>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
